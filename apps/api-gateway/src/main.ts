b89d47b484c24d18b4dfe584876c978e018997ad
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2026-01-18 21:21:09 +0100

    Setup initial api gateway configuration

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -3,15 +3,26 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
