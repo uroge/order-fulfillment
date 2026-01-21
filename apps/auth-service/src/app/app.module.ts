c6d2cf34df9b5f37da25974a07107131ab7ce144
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2026-01-21 20:48:56 +0100

    Implement authentication service with initial setup and middleware integration

--- apps/auth-service/src/app/app.module.ts
+++ apps/auth-service/src/app/app.module.ts
@@ -1,10 +1,45 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import {
+  CorrelationIdMiddleware,
+  CorrelationIdService,
+  JsonLoggerService,
+  RequestLoggingMiddleware,
+} from '@order-fulfillment/shared';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.auth-service'],
+    }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 30,
+        },
+      ],
+    }),
+    AuthModule,
+  ],
+  providers: [
+    CorrelationIdService,
+    JsonLoggerService,
+    RequestLoggingMiddleware,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CorrelationIdMiddleware, RequestLoggingMiddleware)
+      .forRoutes('*');
+  }
+}
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,17 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { JsonLoggerService } from '@order-fulfillment/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const logger = app.get(JsonLoggerService);
+  app.useLogger(logger);
+  const globalPrefix = 'auth';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const config = app.get(ConfigService);
+  const port = config.get<number>('PORT') || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
